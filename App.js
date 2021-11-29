60342c1def171532bf98ad5993ae95b2911d6a20
zhanf164 <hanf.27@osu.edu>
2021-11-29 11:43:21 -0500

    Started working on Uber clone

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { Store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Provider store={Store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false }}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/MapScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+const MapScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>
+                    This is a test.
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
